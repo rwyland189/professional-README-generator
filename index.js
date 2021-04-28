26386895d8aa2869965a1ee3f91c13a6f1f44bd9
Becca Wyland <rwyland@uwalumni.com>
2021-04-27 22:23:43 -0500

    begin writing questions for user input

--- index.js
+++ index.js
@@ -4,7 +4,67 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            // GitHub username
+            type: "input",
+            name: "github",
+            message: "Enter your GitHub username. (Required)",
+            validate: gitHubUsername => {
+                if (gitHubUsername) {
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub username!");
+                    return false;
+                }
+            }
+        },
+        {
+            // Email
+            type: "",
+            name: "",
+            message: ""
+        },
+        {
+            // project title
+            type: "",
+            name: "",
+            message: ""
+        },
+        {
+            // description
+            type: "",
+            name: "",
+            message: ""
+        },
+        {
+            // installation instructions
+            type: "",
+            name: "",
+            message: ""
+        },
+        {
+            // usage information
+            type: "",
+            name: "",
+            message: ""
+        },
+        {
+            // contribution guidelines
+            type: "",
+            name: "",
+            message: ""
+        },
+        {
+            // test instructions
+            type: "",
+            name: "",
+            message: ""
+        },
+        
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
