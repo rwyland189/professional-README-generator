fa9966c4b24ec5a5944cdf1ace22ba86dce0718d
Becca Wyland <rwyland@uwalumni.com>
2021-05-02 17:05:15 -0500

    create variable for question array

--- index.js
+++ index.js
@@ -1,69 +1,82 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-//help: const fs = require("fs");
+const fs = require("fs");
+const util = require("util");
 const generateMarkdown = require("./utils/generateMarkdown");
+const questions = [
+    {
+        // GitHub username
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub username. (Required)",
+        validate: gitHubUsername => {
+            if (gitHubUsername) {
+                return true;
+            } else {
+                console.log("Please enter your GitHub username!");
+                return false;
+            }
+        }
+    },
+    {
+        // Email
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+    {
+        // project title
+        type: "input",
+        name: "title",
+        message: "What is your project's title?"
+    },
+    {
+        // description
+        type: "input",
+        name: "description",
+        message: "Enter a short description of your project."
+    },
+    {
+        // installation instructions
+        type: "input",
+        name: "installation",
+        message: ""
+    },
+    {
+        // license information
+        type: "list",
+        name: "license",
+        message: "",
+        choices: [
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        // usage information
+        type: "input",
+        name: "usage",
+        message: ""
+    },
+    {
+        // contribution guidelines
+        type: "input",
+        name: "contribution",
+        message: ""
+    },
+    {
+        // test instructions
+        type: "input",
+        name: "test",
+        message: ""
+    },
+    
+]
 
 // TODO: Create an array of questions for user input
 const questions = () => {
-    return inquirer.prompt([
-        {
-            // GitHub username
-            type: "input",
-            name: "github",
-            message: "Enter your GitHub username. (Required)",
-            validate: gitHubUsername => {
-                if (gitHubUsername) {
-                    return true;
-                } else {
-                    console.log("Please enter your GitHub username!");
-                    return false;
-                }
-            }
-        },
-        {
-            // Email
-            type: "",
-            name: "",
-            message: ""
-        },
-        {
-            // project title
-            type: "",
-            name: "",
-            message: ""
-        },
-        {
-            // description
-            type: "",
-            name: "",
-            message: ""
-        },
-        {
-            // installation instructions
-            type: "",
-            name: "",
-            message: ""
-        },
-        {
-            // usage information
-            type: "",
-            name: "",
-            message: ""
-        },
-        {
-            // contribution guidelines
-            type: "",
-            name: "",
-            message: ""
-        },
-        {
-            // test instructions
-            type: "",
-            name: "",
-            message: ""
-        },
-        
-    ])
+    return inquirer.prompt(questions)
 };
 
 // TODO: Create a function to write README file
