d199ae55622c375e33274d05204b6972a72930ac
Becca Wyland <rwyland@uwalumni.com>
2021-06-02 20:30:15 -0500

    add license badge to markdown with LA

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,9 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `
+  ![${license} Badge](https://img.shields.io/badge/license-${license}-blue.svg)`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,7 +11,11 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `
+  ## License
+  ${renderLicenseBadge(license)}`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -32,8 +39,7 @@ function generateMarkdown(data) {
   ## Usage
   ${data.usage}
 
-  ## License
-  ${data.license}
+  ${renderLicenseSection(data.license)}
 
   ## Contributing
   ${data.contributing}
